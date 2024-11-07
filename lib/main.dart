67d14ac47c861b1874070f170457903c6f9fb96b
EmadIshaq <emadwagdy1989@gmail.com>
2024-11-08 01:33:48 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const GuideApp());
+}
+
+class GuideApp extends StatelessWidget {
+  const GuideApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
